c09eca955b1371707ea5df2f15495dd03c963a95
Fernando Varela <fervartel@gmail.com>
2019-03-03 12:57:22 -0500

    SSH moved to EC2 module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -142,27 +142,3 @@ resource "aws_route_table_association" "tf-rt-pri" {
     subnet_id = "${aws_subnet.tf-subnet-pri.*.id[count.index]}"
     route_table_id = "${aws_route_table.tf-rt-pri.id}"
 }
-# Custom Security group - SSH
-resource "aws_security_group" "tf-sg-ssh" {
-  name        = "tf-sg-ssh"
-  description = "Terraform created - Allow ssh inbound traffic"
-  vpc_id      = "${aws_vpc.tf-vpc.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "tf-sg-ssh"
-  }
-}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -4,6 +4,3 @@ output "subnets_pri" {
 output "subnets_pub" {
     value = "${aws_subnet.tf-subnet-pub.*.id}"
 }
-output "sg_ssh" {
-    value = "${aws_security_group.tf-sg-ssh.id}"
-}
